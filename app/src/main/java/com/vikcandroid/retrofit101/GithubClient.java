74f38b7540bf66735cafa617978143a1159d9d33
Vikctar <vikc.steve@gmail.com>
2017-07-28 17:09:12 +0300

    Setup an api endpoint (github api)

---
+++ app/src/main/java/com/vikcandroid/retrofit101/GithubClient.java
@@ -0,0 +1,18 @@
+package com.vikcandroid.retrofit101;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by android-dev on 7/28/17.
+ */
+
+public interface GithubClient {
+    @GET("/users/{user}/repos")
+    Call<List<GithubRepo>> userRepos(
+            @Path("user") String user
+    );
+}
---
+++ app/src/main/java/com/vikcandroid/retrofit101/GithubRepo.java
@@ -0,0 +1,21 @@
+package com.vikcandroid.retrofit101;
+
+/**
+ * Created by android-dev on 7/28/17.
+ */
+
+class GithubRepo {
+    private int id;
+    private String name;
+
+    public GithubRepo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
